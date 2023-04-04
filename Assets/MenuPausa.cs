0d651a5668a4adb944e70c278bce4d3184605f8b
DanielCarracedo <110993227+DanielCarracedo@users.noreply.github.com>
2023-04-03 21:55:05 -0500

    Escape

--- Assets/MenuPausa.cs
+++ Assets/MenuPausa.cs
@@ -6,17 +6,29 @@ public class MenuPausa : MonoBehaviour
 {
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menu;
+   private bool detenido=false;
+   public void Update(){
+    if(Input.GetKeyDown(KeyCode.Escape)){
+        if(detenido){
+            RESUMEN();
+        }else{
+            Paused();
+        }
+    }
+   }
     public void Paused(){
+        detenido=true;
         Time.timeScale=0f;
         botonPausa.SetActive(false);
         menu.SetActive(true);
     }
     public void RESUMEN(){
+        detenido=false;
         Time.timeScale=1f;
         botonPausa.SetActive(true);
         menu.SetActive(false);
     }
     public void Quit (){
-
+        Application.Quit();
     }
 }
