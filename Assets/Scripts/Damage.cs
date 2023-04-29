1c098286c8092e6c40d7d73834d675be47581bc1
DanielCarracedo <110993227+DanielCarracedo@users.noreply.github.com>
2023-04-28 22:00:33 -0500

    Sprites

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -2,14 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Daño_vida : MonoBehaviour
+public class Damage : MonoBehaviour
 {
      private void OnCollisionEnter2D(Collision2D collision)
    {
-    if(collision.transform.CompareTag("Jugador"))
-    {
-       
-        Destroy(collision.gameObject);
-    }
+    
    }
 }
---
+++ Assets/Scripts/Player_respawn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_respawn : MonoBehaviour
+{
+    private float checkPintPositionX, checkPintPositionY;
+    void Start(){
+        
+    }
+}
