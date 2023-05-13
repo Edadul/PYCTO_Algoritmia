9f4d8e8ce6cb78b57a8cc5d2c6fe1485820513d9
DanielCarracedo <110993227+DanielCarracedo@users.noreply.github.com>
2023-05-13 18:59:00 -0500

    enemigos #2

--- Assets/Scripts/Mariquita.cs
+++ Assets/Scripts/Mariquita.cs
@@ -1,34 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class Mariquita : MonoBehaviour
 {
       [SerializeField] private float VelocityMov;
-     [SerializeField] private Transform[] puntosMov;
-      [SerializeField] private float Distancia;
-      private int posaleatoria;
-      private SpriteRenderer SP;
-    void Start()
+    [SerializeField] private LayerMask evitar;
+    [SerializeField] private float Distancia;
+    private Rigidbody2D rb2D;
+    public GameObject Sonic;
+    
+   void Start()
     {
-        posaleatoria=Random.Range(0,puntosMov.Length);
-        SP=GetComponent<SpriteRenderer>();
-        Girar();
+        rb2D = GetComponent<Rigidbody2D>();   
     }
     void Update()
     {
-        transform.position=Vector2.MoveTowards(transform.position,puntosMov[posaleatoria].position,VelocityMov*Time.deltaTime);
-        if(Vector2.Distance(transform.position,puntosMov[posaleatoria].position)< Distancia){
-            posaleatoria=posaleatoria=Random.Range(0,puntosMov.Length);
+        rb2D.velocity = new Vector2(VelocityMov * transform.right.x, rb2D.velocity.y);
+        RaycastHit2D informacion = Physics2D.Raycast(transform.position, transform.right, Distancia, evitar);
+        if(informacion)
+        {
             Girar();
         }
 
-    }
-    private void Girar(){
-        if(transform.position.x <puntosMov[posaleatoria].position.x){
-            SP.flipX=true;
-        }else{
-            SP.flipX=false;
+        float dis = Mathf.Abs(Sonic.transform.position.x - transform.position.x);
+        if (dis < 2.0f)
+        {
+            Vector3 direccion = Sonic.transform.position - transform.position;
+            if (direccion.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            atack();
+
         }
+        else
+        {
+
+            Vector3 direccion = Sonic.transform.position - transform.position;
+            if (direccion.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+
+        }
+    }
+    private void Girar()
+    {
+
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+
+    }
+
+    private void atack()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(Sonic.transform.position.x, transform.position.y), VelocityMov * Time.deltaTime);
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * Distancia);
+
     }
 }
