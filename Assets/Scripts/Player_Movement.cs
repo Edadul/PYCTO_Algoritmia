ded048ea8b0ae2b297447d523de493915c8eea7d
Edadul <edadul@uninorte.edu.co>
2023-05-15 10:12:02 -0500

    bnla bla

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,9 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
