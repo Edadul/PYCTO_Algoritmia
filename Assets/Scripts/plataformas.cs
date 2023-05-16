30bfeb834d86e851ac2a078da8e926cc8e28ecc6
Edadul <edadul@uninorte.edu.co>
2023-05-15 22:13:56 -0500

    Plataformas

---
+++ Assets/Scripts/plataformas.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.position.y > gameObject.transform.position.y)
+        {
+            new WaitForSeconds(1f);
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
